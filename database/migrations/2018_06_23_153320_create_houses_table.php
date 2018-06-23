e1f8a51134ba60037cd0795e81233d330fbba4ab
sergio.rodenas <sergio.rodenas@sngular.team>
2018-06-23 17:38:27 +0200

    Add House model and migration
    Also, fix botman class resolution for PHP7.1

---
+++ database/migrations/2018_06_23_153320_create_houses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('name');
+            $table->integer('lat');
+            $table->integer('lng');
+            $table->string('owner');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+}
