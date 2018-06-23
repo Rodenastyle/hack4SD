9b0b41cb91ff9d32dd8b725019282d3570de97d6
Miguel Piedrafita <soy@miguelpiedrafita.com>
2018-06-23 15:56:48 +0200

    Add Twilio conversation

--- routes/botman.php
+++ routes/botman.php
@@ -1,13 +1,21 @@
 <?php
 use BotMan\BotMan\Middleware\ApiAi;
 use App\Http\Controllers\BotManController;
+use App\Conversations\CallConversation;
+use BotMan\Drivers\Twilio\TwilioVoiceDriver;
 
 $botman = resolve('botman');
 
 $botman->middleware->received(ApiAi::create('0434b78f742e417985c091b6ca27bbc1')->listenForAction());
 
+$botman->on(TwilioVoiceDriver::INCOMING_CALL, function ($payload, $bot) {
+    $bot->startConversation(new CallConversation);
+});
+
+
 $botman->hears('{something}', function ($bot, $something) {
     $extras = $bot->getMessage()->getExtras();
+    info([$something, $extras]);
     switch ($extras['apiAction']) {
         case 'some_action':
             // dispatch event
