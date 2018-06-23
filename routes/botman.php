0e6f36d9c284f88bb3c11eb48e02083d3438350a
Miguel Piedrafita <soy@miguelpiedrafita.com>
2018-06-23 15:26:10 +0200

    Start implementing logic for bot

--- routes/botman.php
+++ routes/botman.php
@@ -1,9 +1,24 @@
 <?php
+use BotMan\BotMan\Middleware\ApiAi;
 use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+$botman->middleware->received(ApiAi::create('0434b78f742e417985c091b6ca27bbc1')->listenForAction());
+
+$botman->hears('{something}', function ($bot, $something) {
+    $extras = $bot->getMessage()->getExtras();
+    switch ($extras['apiAction']) {
+        case 'some_action':
+            // dispatch event
+            break;
+
+        case 'some_other_action':
+            // dispatch other event
+            break;
+        
+        default:
+            $bot->reply($extras['apiReply']);
+            break;
+    }
 });
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
