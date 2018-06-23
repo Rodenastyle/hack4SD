09795e4c2c7061cef4ddf9c5c489fbbbfce73071
sergio.rodenas <sergio.rodenas@sngular.team>
2018-06-23 18:19:13 +0200

    Add create_house event and listener
    Start caching minube pois while house is created

---
+++ app/Listeners/CacheHouseNearPointsOfInterest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Support\Facades\Cache;
+
+class CacheHouseNearPointsOfInterest
+{
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+
+    }
+}
--- app/Services/MiNubeService.php
+++ app/Services/MiNubeService.php
@@ -8,18 +8,28 @@
 
 namespace App\Services;
 
+use App\House;
 use GuzzleHttp\Client;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 
-class MiNubeService {
+class MiNubeService
+{
 
-	protected $client, $externalServiceCategoryIdentifiers;
+	protected $client, $externalServiceCategoryIdentifiers, $defaultOptions;
 
 	protected function __construct()
 	{
-		$this->client = new Client();
-		$this->externalServiceCategoryIdentifiers = [
+		$this->client = new Client([
+			'base_uri' => 'http://papi.minube.com'
+		]);
+
+		$this->defaultOptions = [
+			'lang' => 'es',
+			'api_key' => env('MINUBE_API_KEY')
+		];
+
+		$this->externalServiceCategoryIdentifiers = collect([
 			"naturalReserves" => [80, 5, 111, 109, 1, 135, 48, 138, 113, 75, 2, 79, 9, 4, 7, 118, 126, 108, 125],
 			"greenPaths" => [52, 78, 77],
 			"restaurants" => [29, 36, 40, 34, 141, 43, 84, 85, 41],
@@ -27,35 +37,54 @@ class MiNubeService {
 			"privateEstablishments" => [37, 25, 32, 30, 28, 39, 92, 121, 102, 27, 26],
 			"greenActivities" => [47, 49, 48, 53, 61],
 			"extraordinaryPlaces" => [39, 17, 127]
-		];
+		]);
 	}
 
-	public function getNearNaturalReserves() : Collection {
-		//TODO: Complete with house model
-		//Cache::get()
+	public function getNearNaturalReserves(House $house): Collection
+	{
+		return Cache::rememberForever('pois' . $house->id . 'nr', function() use ($house) {
+			return $this->externalServiceCategoryIdentifiers
+				->get("naturalReserves")
+				->map(function($interestPoint) use ($house) {
+					$response = $this->client->get("/pois", [
+						"subcategory_id" => $interestPoint,
+						"latitude" => $house->lat,
+						"longitude" => $house->lng,
+						"max_distance" => '40'
+					])->getBody();
+
+					return $response['name'];
+				});
+		});
 	}
 
-	public function getNearGreenPaths() : Collection {
+	public function getNearGreenPaths(): Collection
+	{
 
 	}
 
-	public function getNearRestaurants() : Collection {
+	public function getNearRestaurants(): Collection
+	{
 
 	}
 
-	public function getNearPrivateEstablishments() : Collection {
+	public function getNearPrivateEstablishments(): Collection
+	{
 
 	}
 
-	public function getNearPublicEstablishments() : Collection {
+	public function getNearPublicEstablishments(): Collection
+	{
 
 	}
 
-	public function getNearGreenActivities() : Collection {
+	public function getNearGreenActivities(): Collection
+	{
 
 	}
 
-	public function getNearExtraordinaryPlaces() : Collection {
+	public function getNearExtraordinaryPlaces(): Collection
+	{
 
 	}
 }
