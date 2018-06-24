09c162bb4384c0c99b18c82414617e514731a01f
Miguel Piedrafita <soy@miguelpiedrafita.com>
2018-06-24 02:19:42 +0200

    Refactor TwilioController to use a normal request flow

--- app/Http/Controllers/TwilioController.php
+++ app/Http/Controllers/TwilioController.php
@@ -2,15 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use Twilio\TwiML;
+use Illuminate\Http\Request;
 use App\Conversations\CallConversation;
+use BotMan\BotMan\Middleware\ApiAi;
+use BotMan\BotMan\Messages\Incoming\IncomingMessage;
 
 class TwilioController extends Controller
 {
     /**
      * Place your Twilio logic here.
      */
-    public function handle($bot, $something)
+    public function handle(Request $request)
     {
-        $bot->startConversation(new CallConversation);
+        $response = new TwiMl;
+
+        if ($request->has('SpeechResult')) {
+            $message = $this->processMessage($request->input('SpeechResult'), $request->input('Caller'));
+
+            info([$message]);
+
+            $response->say($message->getExtras()['apiReply']);
+        } else {
+            $gather = $response->gather(['input' => 'speech', 'timeout' => 10]);
+            $gather->say('Di algo para probar. Tienes 10 segundos.');
+        }
+
+        return $response;
+    }
+
+    public function processMessage($message, $sender)
+    {
+        return ApiAi::create(config('services.dialogflow.key', 'es'))->received(new IncomingMessage($message, $sender, config('botman.twilio.fromNumber')), function($message) {
+            return $message;
+        }, app('botman'));
     }
 }
